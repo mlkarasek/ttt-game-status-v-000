d9874d766340e60113ab8344e95db89149915b68
Monica  Karasek <mlkarasek@gmail.com>
2017-10-01 22:09:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def WIN_COMBINATIONS
-  win_combinations = [[0,1,2],[3,4,5],[6,7,8],[0.4.8],[2,4,6],[0,3,6],[1,4,7],[2.5.8]]
+  win_combinations = [[0,1,2], [3,4,5], [6,7,8], [0.4.8], [2,4,6], [0,3,6], [1,4,7], [2.5.8]]
 
   def won?(board)
 
